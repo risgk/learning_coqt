de746ae24a7d6a3ea66d6cfac44e9996f295c7e1
Ryo Ishigaki <ryo.ishigaki@gmail.com>
2013-04-29 10:38:17 +0900

    fix coq1t

--- coqt1.v
+++ coqt1.v
@@ -7,6 +7,6 @@ Definition prop1 : forall (A B C : Prop), (B -> C) -> (A -> B) -> (A -> C) :=
   fun A B C f g x => f (g x).
 
 Definition ex0 : forall (A B : Prop), A -> (A -> B) -> B :=
-  fun A B x f => f (x).
+  fun A B x f => f x.
 Definition ex1 : forall (A B C : Prop), (A -> B -> C) -> (B -> A -> C) :=
   fun A B C f x y => f y x.
